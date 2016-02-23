cf945f5d73b9797c52add77140099e0414a52411
danylaksono <thalibul.ilm@gmail.com>
2016-02-23 07:53:50 +0700

    initial framework - angular fullstack

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('cuckooApp.admin', [
+  'cuckooApp.auth',
+  'ui.router'
+]);
---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('cuckooApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
